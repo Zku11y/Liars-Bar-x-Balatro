caf0e43fd6c63dc0b3c7fc4dc6a0eb067c4666e6
Mohammed Dakni <mdakni@c2r9p2.1337.ma>
2025-01-24 13:19:17 +0100

    deck is done, clenup code then do game mechanics next

--- liars-bar-x-balatro/Card.gd
+++ liars-bar-x-balatro/Card.gd
@@ -11,22 +11,26 @@ var old_pos
 var mouse_pos_2 = Vector2.ZERO
 var os_vel = 0
 var mouse_vel = Vector2.ZERO
-enum States {IDLE, PICKUP, PLAY}
+enum States {IDLE, PICKUP, TRANSITION, PLAY}
 var state : States = States.IDLE
 var new_state : States
 var pickedup := false
 var screen : Vector2
 var pos_before_pickup
 var card_size = self.size
+var last_state
+var transitioned := false
+const SIZE = Vector2(685 * 0.33, 960 * 0.33)
+
 
 func _ready() -> void:
 	og_scale = scale
 	old_pos = position
 	await get_tree().create_timer(3).timeout
-	pos_before_pickup = self.global_position
+	pos_before_pickup = self.position
 
 func _on_gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion && state == States.IDLE && Globals.active_card == null:
+	if event is InputEventMouseMotion && state == States.IDLE:
 		var mouse_pos = get_local_mouse_position()
 		var diff : Vector2 = (position + size) - mouse_pos
 		var move_dir_x = remap(mouse_pos.x, 0.0, size.x, 0, 1)
@@ -41,28 +45,30 @@ func _on_gui_input(event: InputEvent) -> void:
 
 func _process(delta: float) -> void:
 	screen = get_viewport_rect().size
-	velocity = (position - old_pos) / delta
+	#velocity = (position - old_pos) / delta
+	#os_vel += velocity.normalized().x * 1
+	#var force = (-spring * dp) - (damp * os_vel)
+	#os_vel += force * delta
+	var direction = position - old_pos
+	card.rotation = lerp(card.rotation, direction.x / 100, 0.4)
 	old_pos = position
-	os_vel += velocity.normalized().x * 1
-	var force = (-spring * dp) - (damp * os_vel)
-	os_vel += force * delta
-	dp += os_vel * delta
-	if state == States.IDLE:
-		card.rotation = lerp(dp, 0.0, 1)
-	else:
-		card.rotation = dp
-	#print(self.rotation)
+	#print(state, last_state, transitioned)
+	#if state == States.IDLE:
+		#card.rotation = lerp(card.rotation, 0.0, 1)
+	#else:
+		#dp += os_vel * delta
+		#card.rotation = dp
+	##print(self.rotation)
 	pass
 
 func _physics_process(delta: float) -> void:
-
 	set_state()
 	state_behavior()
 
 
 
 func _on_mouse_entered() -> void:
-	if state != States.IDLE || Globals.active_card:
+	if state == States.PICKUP:
 		return
 	if !pop_up:
 		var tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
@@ -72,7 +78,7 @@ func _on_mouse_entered() -> void:
 
 
 func _on_mouse_exited() -> void:
-	if state != States.IDLE || Globals.active_card:
+	if state != States.IDLE:
 		return
 	var tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
 	tween.tween_property(self, "scale", og_scale - Vector2(0.02, 0.02), 0.5)
@@ -83,33 +89,22 @@ func _on_mouse_exited() -> void:
 
 func set_state():
 	
-
 	if pickedup && Input.is_action_pressed("left_click"):
-		if Globals.active_card == null:
-			Globals.active_card = self
-		elif Globals.active_card != null:
-			return
 		new_state = States.PICKUP
-		#print("picked up")
-	
 	elif Input.is_action_just_released("left_click"):
-		if Globals.active_card == self:
-			Globals.active_card = null
 		new_state = States.IDLE
-		#print("putdown")
-	
-	
-		
+
 
 
 func state_behavior():
-	var last_state := state
+	last_state = state
 	state = new_state
-	if state == States.PICKUP:
+	print(state, " picked up? ", pickedup)
+	if state == States.IDLE:
+		return
+	elif state == States.PICKUP:
 		pickup()
-	elif state == States.IDLE && pos_before_pickup:
-		self.global_position = lerp(self.global_position, pos_before_pickup, 0.1)
-		
+
 
 func pickup():
 	card.material.set_shader_parameter("x_rot", 0)
@@ -122,6 +117,7 @@ func pickup():
 	# Clamp the position to prevent it from moving out of the screen
 	self.global_position.x = clamp(self.global_position.x, -100, screen.x - 100)
 	self.global_position.y = clamp(self.global_position.y, -100, screen.y - 100)
+
 
 
 
--- liars-bar-x-balatro/Card_hand.gd
+++ liars-bar-x-balatro/Card_hand.gd
@@ -1,7 +1,7 @@
 extends ColorRect
 @onready var button: Button = $"../../Button"
 
-const card_scene = preload("res://no_animation_card.tscn")
+const card_scene = preload("res://Cards.tscn")
 var hand_ratio
 var hand = []
 var hand_count = 5
@@ -12,35 +12,36 @@ func _ready() -> void:
 	
 
 func _process(delta: float) -> void:
-	print(self.size)
+	pass
+	#print(self.size)
 
-func draw(n):
-	for i in range(n):
-		var cards = card_scene.instantiate()
-		cards.name = str("card_", i)
-		self.add_child(cards)
-		hand.append(cards)
-		print(hand.size())
-		update_cards()
+func draw():
+	var cards = card_scene.instantiate()
+	cards.name = str("card_", self.get_child_count())
+	self.add_child(cards)
+	hand.append(cards)
+	#print(hand.size())
+	update_cards()
 
 
 
 func update_cards():
 	var cards = hand.size()
-	var cards_space = no_anim.SIZE.x * cards + x_sep * (cards - 1)
+	var cards_space = Card.SIZE.x * cards + x_sep * (cards - 1)
 	var final_sep := x_sep
 	
 	if size.x < cards_space:
-		final_sep = (size.x - (no_anim.SIZE.x * cards)) / (cards - 1)
+		final_sep = (size.x - (Card.SIZE.x * cards)) / (cards - 1)
 		cards_space = size.x
 	
 	var offset = (size.x - cards_space) / 2
-	
-	for i in cards:
-		var card_i = hand[i]
-		card_i.position.x = offset + no_anim.SIZE.x * i + final_sep * i
+	for i in range(cards):
+		var card_i = get_child(i)
+		card_i.position.x = offset + (Card.SIZE.x + final_sep) * i
+		#print("card " ,i , " position = ", card_i.position.x)
+	print(offset, " size of first card = ", hand[0].position.x)
 	pass
 
 
 func _on_button_pressed() -> void:
-	draw(1)
+	draw()
